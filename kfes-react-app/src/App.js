0c967ed50819b3eccd46cb812c5be86fd66a57ce
Aditya Pradita <pradita397@gmail.com>
2019-11-20 14:18:43 +0700

    Add banner component

--- kfes-react-app/src/App.js
+++ kfes-react-app/src/App.js
@@ -13,6 +13,8 @@ function App() {
       <div class="k-festival-wrap">
       <Helmet>
           <title>iLOTTE K-Festival</title>
+          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
       </Helmet>
       <Header></Header>
 
@@ -21,6 +23,7 @@ function App() {
                 <div className="tab-content">
                     <div role="tabpanel" className="tab-pane active" id="home">
                       <div class="col-12 content no-pad">
+                        <Banners></Banners>
                         <EventList></EventList>
                         <EventCalendar></EventCalendar>
 
--- kfes-react-app/src/Banners.js
+++ kfes-react-app/src/Banners.js
@@ -1,5 +1,13 @@
 import React, {Component} from 'react'; 
 import Slider from "react-slick";
+import $ from 'jquery';
+
+import Carousel from 'react-bootstrap/Carousel';
+
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
+const bootstrap = require('bootstrap');
 
 function NextArrow(props) {
     return (
@@ -20,44 +28,39 @@ function NextArrow(props) {
 class Banners extends Component {
     render()
     {
-        var settings = {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: false,
-            arrows: true,
-            dots: true,
-            prevArrow: <PrevArrow />,
-            nextArrow: <NextArrow />
-        };
         return (
-            <section className="col-12 section section-banner">
-                <Slider {...settings} className="col-12 banner-slide">
-                <a href="#k-content-expo-2019" title="K-Content Expo 2019">
-                    <div className="banner-slide-item" align="center">
-                        <img className="bg-slider" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/billboards/mbb_kcontent.jpg" width="100%" alt="K-Content Event Billboard" />
-                    </div>
-                </a>
-
-                <a href="#seoul-food-from-home" title="Seoul Food From Home">
-                    <div className="banner-slide-item" align="center">
-                        <img className="bg-slider" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/billboards/mbb_kfood.jpg" width="100%" alt="Seoul Food From Home" />
-                    </div>
-                </a>
-                
-                <a href="#hangul-day" title="Hangul Day">
-                    <div className="banner-slide-item" align="center">
-                        <img className="bg-slider" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/billboards/mbb_hangulday.jpg" width="100%" alt="Hangul Day Event Billboard" />
-                    </div>
-                </a>
-
-                <a href="#korea-indonesia-film-festival" title="Korea Indonesia Film Festival">
-                    <div className="banner-slide-item" align="center">
-                        <img className="bg-slider" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/billboards/mbb_koreanculture.jpg" width="100%" alt="Korea - Indonesia 10th Film Festival Billboard" />
-                    </div>
-                </a>
-                </Slider>                    
+            <section className="col-xs-12 section section-banner">
+                <Carousel>
+                <Carousel.Item>
+                    <a href="#korea-indonesia-film-festival" title="Korea Indonesia Film Festival">
+                        <div className="banner-slide-item" align="center">
+                            <img className="bg-slider" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/billboards/mbb_koreanculture.jpg" width="100%" alt="Korea - Indonesia 10th Film Festival Billboard" />
+                        </div>
+                    </a>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <a href="#hangul-day" title="Hangul Day">
+                            <div className="banner-slide-item" align="center">
+                                <img className="bg-slider" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/billboards/mbb_hangulday.jpg" width="100%" alt="Hangul Day Event Billboard" />
+                            </div>
+                        </a>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <a href="#seoul-food-from-home" title="Seoul Food From Home">
+                            <div className="banner-slide-item" align="center">
+                                <img className="bg-slider" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/billboards/mbb_kfood.jpg" width="100%" alt="Seoul Food From Home" />
+                            </div>
+                        </a>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <a href="#k-content-expo-2019" title="K-Content Expo 2019">
+                            <div className="banner-slide-item" align="center">
+                                <img className="bg-slider" src="https://ecm-prd-pub.s3.ap-southeast-1.amazonaws.com/kfestival/assets/img/billboards/mbb_kcontent.jpg" width="100%" alt="K-Content Event Billboard" />
+                            </div>
+                        </a>
+                    </Carousel.Item>
+                </Carousel>
             </section>
-            
         )
     }
 }
