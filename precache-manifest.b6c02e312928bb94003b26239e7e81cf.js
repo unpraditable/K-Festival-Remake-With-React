41984f12379a4ee8da0a4a54be725c4f0a8f9ead
Aditya Pradita <pradita397@gmail.com>
2020-02-17 14:00:45 +0700

    Updates

---
+++ precache-manifest.b6c02e312928bb94003b26239e7e81cf.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ada6fe4391e6188558b961e2d1fa8c",
+    "url": "/K-Festival-Remake-With-React/index.html"
+  },
+  {
+    "revision": "1095fcdc92c3a8437860",
+    "url": "/K-Festival-Remake-With-React/static/css/2.58c81cd9.chunk.css"
+  },
+  {
+    "revision": "58c91be68fff4e2b001f",
+    "url": "/K-Festival-Remake-With-React/static/css/main.6fe6ff29.chunk.css"
+  },
+  {
+    "revision": "1095fcdc92c3a8437860",
+    "url": "/K-Festival-Remake-With-React/static/js/2.952d62ab.chunk.js"
+  },
+  {
+    "revision": "58c91be68fff4e2b001f",
+    "url": "/K-Festival-Remake-With-React/static/js/main.4f39ea02.chunk.js"
+  },
+  {
+    "revision": "c31f8d2d19ab8ae8b1a9",
+    "url": "/K-Festival-Remake-With-React/static/js/runtime-main.967a113b.js"
+  },
+  {
+    "revision": "5e07e40031bfefc2e0b77971e1ee93e2",
+    "url": "/K-Festival-Remake-With-React/static/media/DIN_Regular.5e07e400.eot"
+  },
+  {
+    "revision": "7b420ffd0c09f38cf38b17a8533f6936",
+    "url": "/K-Festival-Remake-With-React/static/media/DIN_Regular.7b420ffd.woff"
+  },
+  {
+    "revision": "e0185b8dd98e7dadde77c95bcc0e8b8d",
+    "url": "/K-Festival-Remake-With-React/static/media/DIN_Regular.e0185b8d.ttf"
+  },
+  {
+    "revision": "faaa37bd5253b2467e0e18ff46374aaf",
+    "url": "/K-Festival-Remake-With-React/static/media/DIN_Regular.faaa37bd.otf"
+  },
+  {
+    "revision": "21140248d8dda0109003bbcfc50a2de2",
+    "url": "/K-Festival-Remake-With-React/static/media/Gibson-Regular.21140248.ttf"
+  },
+  {
+    "revision": "5436eb62561957fd9d8522530aa72113",
+    "url": "/K-Festival-Remake-With-React/static/media/Gibson-SemiBold.5436eb62.otf"
+  },
+  {
+    "revision": "423d9d3957eb8bd862e2b253f694405e",
+    "url": "/K-Festival-Remake-With-React/static/media/Gotham-Medium.423d9d39.woff"
+  },
+  {
+    "revision": "c9f7f5fbf27d0712643cf5d0738ae903",
+    "url": "/K-Festival-Remake-With-React/static/media/Gotham-Medium.c9f7f5fb.otf"
+  },
+  {
+    "revision": "32292b2faf73a06f5f85350ba1a18cbd",
+    "url": "/K-Festival-Remake-With-React/static/media/HelveticaNeueLTStd-Md.32292b2f.woff"
+  },
+  {
+    "revision": "e307f991c441ef885714c96f5c0a53f4",
+    "url": "/K-Festival-Remake-With-React/static/media/HelveticaNeueLTStd-Md.e307f991.ttf"
+  },
+  {
+    "revision": "f0d7d673ef06ec9cf090dc00572d63e4",
+    "url": "/K-Festival-Remake-With-React/static/media/HelveticaNeueLTStd-Md.f0d7d673.otf"
+  },
+  {
+    "revision": "3962ec3bda82344439b29d9a3d54d42e",
+    "url": "/K-Festival-Remake-With-React/static/media/KGSecondChancesSolid.3962ec3b.ttf"
+  },
+  {
+    "revision": "d26f1cdb370e33d4d794b91a298b52fa",
+    "url": "/K-Festival-Remake-With-React/static/media/KGSecondChancesSolid.d26f1cdb.eot"
+  },
+  {
+    "revision": "d3a5003e9f6e18bfc6ad37bffedd2bf0",
+    "url": "/K-Festival-Remake-With-React/static/media/KGSecondChancesSolid.d3a5003e.woff"
+  },
+  {
+    "revision": "e7f249e71a563eed9c495835657eb9c2",
+    "url": "/K-Festival-Remake-With-React/static/media/PoetsenOne-Regular.e7f249e7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/K-Festival-Remake-With-React/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/K-Festival-Remake-With-React/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/K-Festival-Remake-With-React/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/K-Festival-Remake-With-React/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/K-Festival-Remake-With-React/precache-manifest.b6c02e312928bb94003b26239e7e81cf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/K-Festival-Remake-With-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
